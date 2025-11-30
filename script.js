3ecd9bb46687a8526d9a4050b7307f4fb475ab37
Noah <noohxd8@gmail.com>
2025-11-30 17:03:05 +0200

    change the description to a more accurate comment

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// ------ Painting Icon ------:
+// ------ Handle primary color picker toggle ------:
 const colorIcon = document.getElementById("colorIcon");
 const colorPicker = document.getElementById("colorPicker");
 
 colorIcon.addEventListener("click", () => {
-    colorPicker.classList.toggle("active");
+  colorPicker.classList.toggle("active");
 });
 
 colorPicker.addEventListener("input", (e) => {
-    const newColor = e.target.value;
-    const body = document.querySelector("body");
+  const newColor = e.target.value;
+  const body = document.querySelector("body");
 
-    body.style.setProperty("--primary-blue", newColor);
+  body.style.setProperty("--primary-blue", newColor);
 });
 
 // ------ Themes ------:
 const themeToggling = document.getElementById("theme");
 
 themeToggling.addEventListener("click", () => {
-    const body = document.querySelector("body");
-    body.classList.toggle("dark-theme");
+  const body = document.querySelector("body");
+  body.classList.toggle("dark-theme");
 });
 
 // ------ Search Icon ------:
@@ -26,7 +26,7 @@ const searchIcon = document.getElementById("searchIcon");
 const searchInput = document.getElementById("searchInput");
 
 searchIcon.addEventListener("click", () => {
-    searchInput.classList.toggle("active");
+  searchInput.classList.toggle("active");
 });
 
 // ------ Hamburger Icon ------:
@@ -35,9 +35,9 @@ const mobileMenu = document.getElementById("mobileMenu");
 const closeMenu = document.getElementById("closeMenu");
 
 hamburger.addEventListener("click", () => {
-    mobileMenu.classList.add("active");
+  mobileMenu.classList.add("active");
 });
 
 closeMenu.addEventListener("click", () => {
-    mobileMenu.classList.remove("active");
+  mobileMenu.classList.remove("active");
 });
